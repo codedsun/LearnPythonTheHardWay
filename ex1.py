021e10cf4d54444a6421c0d53568e0db538ec262
Suneet Srivastava <suneetbond91@gmail.com>
2018-03-12 15:41:53 +0530

    Ex1Done

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print ("Hello world!")
+print ("Hello again")
+print ("I like typing this")
+print ("This is fun!")
+print ("Yay, this is printing ")
+print ("I'd much rather you not.")
+print ('I "said" dont touch this.')
