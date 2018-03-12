b475776aa55d6ccf23abd9b35e44ccae7b7f0aa8
Suneet Srivastava <suneetbond91@gmail.com>
2018-03-12 18:27:29 +0530

    Ex5done

---
+++ ex5.py
@@ -0,0 +1,11 @@
+my_name = 'Suneet'
+my_age = 20 #this is good
+my_height = '5.9' #feet
+my_weight = 50
+my_eyes = "Black"
+my_teeth = "White"
+my_hair = "White" #sounds wierd
+
+print ("Lets talk about %s"%my_name)
+print ("He's $s tall"% my_height)
+print ("He's $s heavy" $ my_weight)
