c90915f154c0b06faeee3a1c31f5727fc74bbbb9
Suneet Srivastava <suneetbond91@gmail.com>
2018-03-13 16:18:04 +0530

    ex21Done

---
+++ ex21.py
@@ -0,0 +1,21 @@
+#function with return value
+
+def add (a,b):
+    print("Adding")
+    return a+b
+def subtract(a,b):
+    print("Subtracing")
+    return a-b
+
+def multiply(a,b):
+    print("Mutiplying")
+    return a*b
+
+def divide(a,b):
+    print("Dividing")
+    return a/b;
+
+print(add(1,2))
+print(subtract(1,2))
+print(multiply(1,2))
+print(divide(1,2))
