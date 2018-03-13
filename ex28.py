dd57a8064c34e580d7dcd5c911f9c4a3672e244f
Suneet Srivastava <suneetbond91@gmail.com>
2018-03-13 19:34:57 +0530

    ex28Done

---
+++ ex28.py
@@ -0,0 +1,18 @@
+#Exercise 28
+
+print(True and False)
+print(False and True)
+print(1==1 and 2==1)
+print("test" == "test")
+print(1==1 or 2!=1)
+print(True and 1==1)
+print(False and 0!=0)
+print(True and 1==1)
+print(False and 0!=0)
+print(True or 1==1)
+print("test" == "testing")
+print(1!=0 and 2==1)
+print("test"==1)
+print(not(True and False))
+print(not(1==1))
+print(3 == 3 and not ("testing" == "testing" or "Python" == "Fun"))
